6a880bd0758f4f88b5f9dbb49baf647f71b94137
rolpppp <rlgarces@up.edu.ph>
2025-09-20 19:04:32 +0800

    successfully set up maven project

---
+++ JeoPyramid/src/main/java/com/noinc/bloomsjeopardy/App.java
@@ -0,0 +1,13 @@
+package com.noinc.bloomsjeopardy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
