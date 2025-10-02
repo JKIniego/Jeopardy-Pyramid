5b956d5b6ff7af07fdb851b371987ce13f1b4e75
Schneidelstrom <jipetilla@up.edu.ph>
2025-10-02 10:37:15 +0800

    Small revision and versioning

--- JeoPyramid/src/main/java/com/noinc/bloomsjeopardy/App.java
+++ JeoPyramid/src/main/java/com/noinc/bloomsjeopardy/App.java
@@ -2,10 +2,6 @@ package com.noinc.bloomsjeopardy;
 
 import com.noinc.bloomsjeopardy.controller.GameEngine;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main( String[] args )
     {
